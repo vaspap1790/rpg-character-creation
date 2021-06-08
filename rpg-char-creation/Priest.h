20648744889123e99abc715ecb553dd1aef87efe
vaspap1790 <vaspap1790@gmail.com>
2021-06-08 11:28:44 +0300

    implemented

---
+++ rpg-char-creation/Priest.h
@@ -0,0 +1,23 @@
+#ifndef PRIEST_H
+#define PRIEST_H
+
+#include "Player.h"
+
+class Priest : public Player
+{
+public:
+	Priest(string name, Race race) : Player(name, race, 100, 200)
+	{
+	}
+
+	string attack() const
+	{
+		return "I will assault you with Holy Wrath!";
+	}
+
+	string getSpecialty() const
+	{
+		return "Priest";
+	}
+};
+#endif 
